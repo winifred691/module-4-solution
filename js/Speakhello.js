4b6d54dbc6841e0d26bf359db9e22d1fee79d3bf
winifred <66266116+winifred691@users.noreply.github.com>
2022-08-28 23:04:14 -0700

    Update Speakhello.js

--- js/Speakhello.js
+++ js/Speakhello.js
@@ -6,6 +6,7 @@
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 (function (window) {
+  var helloSpeaker = {};
 helloSpeaker.speak = {}; 
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
